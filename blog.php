80bb56298fb411491b01854620a08e6438de7c32
tonicrr <tonicrr@gmail.com>
2016-10-15 20:11:42 +0200

    change compartir facebook button

--- blog.php
+++ blog.php
@@ -32,8 +32,8 @@
                         </div>
                         <h4 style="margin-top: 15px;">Mezclado, no agitado</h4>
                         <p>En este post os hablaré de los distintos tipos de eventos que puedo organizar. No todo va a ser bodas especiales, ni comuniones bonitas o eventos corporativos.</p>
-                        <!-- Go to www.addthis.com/dashboard to customize your tools -->
-                        <div class="addthis_native_toolbox" style="float: right; padding-top: 1rem;"></div>
+                        <!-- facebook share -->
+                        <div class="fb-share-button" data-href="http://www.pcstandm.com/blog/blog3.php" data-layout="button_count" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.pcstandm.com%2Fblog%2Fblog3.php&amp;src=sdkpreparse">Compartir</a></div>
                         <a id="link-blog1" href="blog/blog3.php"><p class="padding-top" style="color: #fbd294; text-align: center;">Leer mas...</p></a>
                     </div>
                 </section>
@@ -47,8 +47,8 @@
                         <div style="float: right;"><p id="blog-2-views" style="text-align: right; font-size: 12px; font-style: italic; float: left;"></p><i class="fa fa-eye fa-1x" style="color: black; float: right; padding: 5px;"></i></div>
                         <h4 style="margin-top: 15px;">Ser cuentialumna</h4>
                         <p>En noviembre del pasado año estaba de camino a Zaragoza (No a Tarragona Mama…) para el curso de Bodas de cuento. </br></br>¡Un fin de semana fuera de lo normal para aprender y disfrutar de una experiencia genial!</p>
-                        <!-- Go to www.addthis.com/dashboard to customize your tools -->
-                        <div class="addthis_native_toolbox" style="float: right; padding-top: 1rem;"></div>
+                        <!-- facebook share -->
+                        <div class="fb-share-button" data-href="http://www.pcstandm.com/blog/blog2.php" data-layout="button_count" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.pcstandm.com%2Fblog%2Fblog2.php&amp;src=sdkpreparse">Compartir</a></div>
                         <a id="link-blog1" href="blog/blog2.php"><p class="padding-top" style="color: #fbd294; text-align: center;">Leer mas...</p></a>
                     </div>
                 </section>
@@ -62,8 +62,8 @@
                         <div style="float: right;"><p id="blog-1-views" style="text-align: right; font-size: 12px; font-style: italic; float: left;"></p><i class="fa fa-eye fa-1x" style="color: black; float: right; padding: 5px;"></i></div>
                         <h4 style="margin-top: 15px;">Visita Mireia Vidal Barcelona</h4>
                         <p>El día de la visita a Mireia Vidal Barcelona fue un día con muchas sorpresas. Salí de la ofi’ de camino a Barcelona para encontrar-me con una joven diseñadora de vestidos de novia y de fiesta que abrió su negocio hace apenas un año, y que ya está dando mucho de qué hablar gracias a su trabajo y esfuerzo. Pese al poco tiempo que lleva en el mercado, tiene oficinas en Nueva York, Méjico y Londres.</p>
-                        <!-- Go to www.addthis.com/dashboard to customize your tools -->
-                        <div class="addthis_native_toolbox" style="float: right; padding-top: 1rem;"></div>
+                        <!-- facebook share -->
+                        <div class="fb-share-button" data-href="http://www.pcstandm.com/blog/blog1.php" data-layout="button_count" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.pcstandm.com%2Fblog%2Fblog1.php&amp;src=sdkpreparse">Compartir</a></div>
                         <a id="link-blog1" href="blog/blog1.php"><p class="padding-top" style="color: #fbd294; text-align: center;">Leer mas...</p></a>
                     </div>
                 </section>
@@ -96,6 +96,13 @@
                 });
             });
         </script>
-        <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-525fedc129c21605"></script>
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.8";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
     </body>
 </html>
